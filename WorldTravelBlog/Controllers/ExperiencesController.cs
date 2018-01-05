fc56914705bde9c6a0c35488de45a6e13b83817c
Daniel Cheng and Sravyy Velaga <sravyy@>
2018-01-04 16:30:40 -0800

    add experiences controller and full views

--- WorldTravelBlog/Controllers/ExperiencesController.cs
+++ WorldTravelBlog/Controllers/ExperiencesController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WorldTravelBlog.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +13,67 @@ namespace WorldTravelBlog.Controllers
 {
     public class ExperiencesController : Controller
     {
-        // GET: /<controller>/
+        private WorldTravelBlogContext db = new WorldTravelBlogContext();
+
         public IActionResult Index()
         {
+            var thisExperience = db.Experiences
+                               .Include(x => x.Location)
+                               .ToList();
+
+            return View(thisExperience);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var thisExperience = db.Experiences
+                               .Include(x => x.Location)
+                               .FirstOrDefault(x => x.ExperienceId == id);
+            return View(thisExperience);
+        }
+
+        public IActionResult Create()
+        {
+            ViewBag.LocationId = new SelectList(db.Locations, "LocationId", "City");
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Experience experience)
+        {
+            db.Experiences.Add(experience);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var thisExperience = db.Experiences.FirstOrDefault(x => x.ExperienceId == id);
+            ViewBag.LocationId = new SelectList(db.Locations, "LocationId", "City");
+            return View(thisExperience);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Experience experience)
+        {
+            db.Entry(experience).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var thisExperience = db.Experiences.FirstOrDefault(x => x.ExperienceId == id);
+            return View(thisExperience);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var thisExperience = db.Experiences.FirstOrDefault(x => x.ExperienceId == id);
+            db.Experiences.Remove(thisExperience);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
